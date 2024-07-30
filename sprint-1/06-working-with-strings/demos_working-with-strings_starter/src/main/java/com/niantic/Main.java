02e1f72cbece4cc1e7f62911c53220c32426b131
Karen Delarosa <kaydelarose@gmail.com>
2024-07-29 17:41:20 -0700

    follow along demo

--- sprint-1/06-working-with-strings/demos_working-with-strings_starter/src/main/java/com/niantic/Main.java
+++ sprint-1/06-working-with-strings/demos_working-with-strings_starter/src/main/java/com/niantic/Main.java
@@ -4,6 +4,23 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+
+        String formattedPhone = formatPhone(originalPhone: "(808)555-1234")
+        System.out.println(formattedphone);
+    }
+
+    public static String formatPhone(String originalPhone) {
+        char[] chars = new char[10];
+
+        for (int i = 0; i < originalPhone.length(); i++)
+        {
+            char currentChar = originalPhone.charAt(i);
+            //if(Character.isDigit(currentChar))
+            if (currentChar >= '0' && currentChar <= "9") {
+                Character isDigit(currentChar);
+            }
+            
+        }
+        return new String(chars);
     }
 }
