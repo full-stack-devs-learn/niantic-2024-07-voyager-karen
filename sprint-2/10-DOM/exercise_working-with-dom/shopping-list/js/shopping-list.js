ce736d431507beeda242551ee5386b5850d0f56c
Karen Delarosa <kaydelarose@gmail.com>
2024-08-30 16:56:13 -0700

    finished dom exercise

--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
@@ -2,22 +2,81 @@
 
 // add groceries
 
+// create instance of service
+const service = new ShoppingService();
+
+
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function displayListTitle() {}
+function displayListTitle() {
+
+    const listTitle = document.getElementById("title");
+    listTitle.textContent = service.getListName();
+
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() {
 
-/**
- * This function will be called when the button is clicked. You will need to get a reference
- * to every list item and add the class completed to each one
- */
+    const groceries = service.getShoppingList();
+
+    const groceryListContainer = document.getElementById("shopping-list");
+
+
+    groceries.forEach(grocery => {
+
+        // create a new div element for each grocery item
+        const itemDiv = document.createElement('div');
+
+        itemDiv.classList.add('list-item');
+
+        // create title span
+        const titleSpan = document.createElement('span');
+        titleSpan.textContent = grocery.title;
+
+        // create quantity container div
+        const quantityContainer = document.createElement('div');
+        quantityContainer.classList.add('quantity-container');
+
+        // create 'super' span
+        const superSpan = document.createElement('span');
+        superSpan.classList.add('super');
+        superSpan.textContent = 'quantity';
+
+        // create text node for the quantity
+        const quantityText = document.createTextNode(grocery.quantity);
+
+        // append span and quantity to quantity container
+        quantityContainer.appendChild(superSpan);
+        quantityContainer.appendChild(quantityText);
+
+        // append title and container to items 
+        itemDiv.appendChild(titleSpan);
+        itemDiv.appendChild(quantityContainer);
+        
+        // append grocery item div to the shopping list container
+        groceryListContainer.appendChild(itemDiv);
+
+
+    });
+
+}
+
+
 function markCompleted() {
+
+    const listItems = document.querySelectorAll('.list-item');
+
+    listItems.forEach(item => {
+
+        item.classList.add('complete');
+
+    });
+
 }
 
 
--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
@@ -1,22 +1,73 @@
 class ShoppingService {
 
     getListName() {
+
+        return "Kitty's Shopping List";
+
     }
 
     getShoppingList() {
         return [
             {
                 id: 1,
-                title: '5 lb sack of potatoes',
-                quantity: 1,
+                title: 'Red Snapper',
+                quantity: 3,
                 isComplete: false
             },
             {
                 id: 2,
-                title: 'Frozen Pizza',
+                title: 'Honeycrisp Apples',
                 quantity: 4,
                 isComplete: true
-            }
+            },
+            {
+                id: 3,
+                title: 'Jasmine Rice',
+                quantity: 1,
+                isComplete: true
+            },
+            {
+                id: 4,
+                title: 'Green Onion',
+                quantity: 2,
+                isComplete: true
+            },
+            {
+                id: 5,
+                title: 'Bag of Spinach',
+                quantity: 1,
+                isComplete: true
+            },
+            {
+                id: 6,
+                title: 'Assorted Colorful Carrots',
+                quantity: 1,
+                isComplete: false
+            },
+            {
+                id: 7,
+                title: 'Fish Sauce',
+                quantity: 1,
+                isComplete: false
+            },
+            {
+                id: 8,
+                title: 'Tofu',
+                quantity: 1,
+                isComplete: true
+            },
+            {
+                id: 9,
+                title: 'Lobster',
+                quantity: 2,
+                isComplete: true
+            },
+            {
+                id: 10,
+                title: 'Egg Noodles',
+                quantity: 4,
+                isComplete: true
+            },
         ];
     }
 }
