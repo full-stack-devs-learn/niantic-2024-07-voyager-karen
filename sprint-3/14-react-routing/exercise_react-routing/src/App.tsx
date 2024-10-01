e4f0c8d47e1a465f449e1fa7c4520d1ce94c625a
Karen Delarosa <kaydelarose@gmail.com>
2024-10-01 08:08:02 -0700

    updated header / home page

--- sprint-3/14-react-routing/exercise_react-routing/src/App.tsx
+++ sprint-3/14-react-routing/exercise_react-routing/src/App.tsx
@@ -3,13 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
 import CategoriesList from './components/categories/CategoriesList';
-import CategoryDetails from './components/categories/CategoryDetails';
-import CategoryAdd from './components/categories/CategoryAdd';
-import CategoryEdit from './components/categories/CategoryEdit';
-import ProductSearch from './components/products/ProductSearch';
-import ProductDetails from './components/products/ProductDetails';
-import ProductAdd from './components/products/ProductAdd';
-import ProductEdit from './components/products/ProductEdit';
+// import CategoryDetails from './components/categories/CategoryDetails';
+// import CategoryAdd from './components/categories/CategoryAdd';
+// import CategoryEdit from './components/categories/CategoryEdit';
+// import ProductSearch from './components/products/ProductSearch';
+// import ProductDetails from './components/products/ProductDetails';
+// import ProductAdd from './components/products/ProductAdd';
+// import ProductEdit from './components/products/ProductEdit';
 
 const App: React.FC = () => {
   return (
@@ -18,13 +18,13 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/categories" element={<CategoriesList />} />
-        <Route path="/categories/add" element={<CategoryAdd />} />
+        {/* <Route path="/categories/add" element={<CategoryAdd />} />
         <Route path="/categories/:id" element={<CategoryDetails />} />
         <Route path="/categories/:id/edit" element={<CategoryEdit />} />
         <Route path="/products" element={<ProductSearch />} />
         <Route path="/products/add" element={<ProductAdd />} />
         <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="/products/:id/edit" element={<ProductEdit />} />
+        <Route path="/products/:id/edit" element={<ProductEdit />} /> */}
       </Routes>
     </div>
   );
---
+++ sprint-3/14-react-routing/exercise_react-routing/src/components/categories/CategoriesList.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CategoriesList: React.FC = () => {
+  return <div>Categories List</div>;
+};
+
+export default CategoriesList; // Make sure it's a default export
